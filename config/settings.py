c793e314d0ecb81af4be5607854095b556a55ba8
Justprince234 <oilsunny9@gmail.com>
2023-07-31 15:01:19 +0000

    Almost done1.

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-
+import mimetypes
 import dj_database_url
 
 from dotenv import load_dotenv
@@ -151,6 +151,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+mimetypes.add_type("text/css", ".css", True)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
